c28be74738e9acd5e8b5692294525f461675a7af
c9sanbal <c9sanbal@SZT3-7-W10>
2025-02-20 09:19:03 +0100

    A main Window-ban kesz van a menü

--- OrderProject/ViewModel/MegrendelesViewModel.cs
+++ OrderProject/ViewModel/MegrendelesViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,11 @@ using System.Threading.Tasks;
 
 namespace OrderProject.ViewModel
 {
-    public class MegrendelesViewModel
+    public partial class MegrendelesViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private object _currentView = new object();
+
 
     }
 }
